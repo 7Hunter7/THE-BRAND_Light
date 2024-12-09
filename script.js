3ea7088e0516e0f9e5ea3e5f450a8be0e49e2702
Hunter <i.am.hunter.777.ih@gmail.com>
2024-12-09 12:59:54 +0300

    Updated 'script.js' - Added function 'toggleMenu'. Renamed itams and added item checks

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const menuActive = document.querySelector("div.menu");
-const menuClose = menuActive.querySelector("button.menu__close");
-const menuButton = document.querySelector("a.menu__button");
-const topEl = document.querySelector("div.top");
+// Переменные
+const menuContainer = document.querySelector("div.menu");
+const menuClose = menuContainer.querySelector("button.menu__close");
+const menuOpenButton = document.querySelector("a.menu__button");
+const topSection = document.querySelector("div.top");
 
-menuButton.addEventListener("click", function (e) {
-  menuActive.classList.toggle("hidden");
-  topEl.classList.toggle("top__image_menu");
-});
+// Функция обработки событий
+const toggleMenu = () => {
+  menuContainer.classList.toggle("hidden");
+  topSection.classList.toggle("top__image_menu");
+};
 
-menuClose.addEventListener("click", function (e) {
-  menuActive.classList.toggle("hidden");
-  topEl.classList.toggle("top__image_menu");
-});
+// Проверка наличия элементов
+if (menuOpenButton && menuContainer && menuClose) {
+  menuOpenButton.addEventListener("click", toggleMenu);
+  menuClose.addEventListener("click", toggleMenu);
+}
