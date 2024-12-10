52889af5219ba407953f4d271daa1ff1ead5cb84
Hunter <i.am.hunter.777.ih@gmail.com>
2024-12-10 08:23:34 +0300

    Updated 'script.js' - Added new function 'closeMenuOnOutsideClick'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Переменные
 const menuContainer = document.querySelector("div.menu");
-const menuClose = menuContainer.querySelector("button.menu__close");
+const menuClose = menuContainer?.querySelector("button.menu__close");
 const menuOpenButton = document.querySelector("a.menu__button");
 const topSection = document.querySelector("div.top");
 
@@ -10,8 +10,20 @@ const toggleMenu = () => {
   topSection.classList.toggle("top__image_menu");
 };
 
+// Функция закрытия меню при клике вне его области
+const closeMenuOnOutsideClick = (event) => {
+  if (
+    !menuContainer.classList.contains("hidden") &&
+    !menuContainer.contains(event.target) &&
+    !menuOpenButton.contains(event.target)
+  ) {
+    toggleMenu();
+  }
+};
+
 // Проверка наличия элементов
 if (menuOpenButton && menuContainer && menuClose) {
   menuOpenButton.addEventListener("click", toggleMenu);
   menuClose.addEventListener("click", toggleMenu);
+  document.addEventListener("click", closeMenuOnOutsideClick);
 }
